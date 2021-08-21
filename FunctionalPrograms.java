965e91415b857161729e38b4961da5c1d4b4dc5c
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-22 03:53:16 +0530

    Euclidean distance

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -3,45 +3,21 @@ package com.bridgelabz.FunctionalPrograms;
 import java.util.Scanner;
 public class FunctionalPrograms 
 {
-	public static int findDistinctTriplets(int b[],int length)
-	{
-		System.out.println();
-		int i, j, k, count=0;
-		for(i=0; i<length; i++)
-		{
-			for(j=i+1; j<length; j++)
-			{
-				for(k=j+1; k<length; k++)
-				{
-					if (b[i] + b[j] + b[k] == 0)
-					{
-						System.out.println(b[i] + " " + b[j] + " " + b[k]);
-						count++;
-					}
-				}
-			}
-		}
-		return count;
+	static void calculate(int x, int y) {
+		System.out.println("Euclidean distance is " + Math.pow((x * x + y * y), 0.5));
 	}
 
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println();
-
-		System.out.print("Enter Size of Array :");
-		int size = sc.nextInt();
-		int arr[] = new int [size];
-
-		for(int i=0; i<arr.length; i++)
-		{
-			System.out.print("Enter "+(i+1)+" Value :");
-			arr[i] = sc.nextInt();
+	public static void main(String[] args) {
+		try {
+			Scanner s = new Scanner(System.in);
+			System.out.println("Enter the value for x");
+			int x = s.nextInt();
+			System.out.println("Enter the value for y ");
+			int y = s.nextInt();
+			calculate(x, y);
+			s.close();
+		} catch (Exception e) {
+			System.out.println("Enter integer only");
 		}
-
-		FunctionalPrograms t1 = new FunctionalPrograms();
-		int sum = t1.findDistinctTriplets(arr,size);
-		System.out.println("No.of Tripltes :"+sum);
-		System.out.println();
 	}
 }
