9590dac236065f79d3613e350524ba65f3fd014c
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-22 04:06:56 +0530

    prints the wind chill

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -3,25 +3,26 @@ package com.bridgelabz.FunctionalPrograms;
 import java.util.Scanner;
 public class FunctionalPrograms 
 {
-	static void roots(int a, int b, int c) {
-		int delta = Math.abs(b * b - 4 * a * c);
-		double x1 = (-b + Math.pow(delta, 1 / 2)) / (2 * a);
-		double x2 = (-b - Math.pow(delta, 1 / 2)) / (2 * a);
-		System.out.println(x1);
-		System.out.println(x2);
+	static void windChill(double f, double ws) {
+
+		double w = 35.74 + 0.62158 * f + (0.4275 * f - 35.75) * Math.pow(ws, 0.16);
+		System.out.println(w);
 	}
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		try {
-			System.out.println("Enter the value of a ");
-			int a = s.nextInt();
-			System.out.println("Enter the value of b");
-			int b = s.nextInt();
-			System.out.println("Enter the value of c");
-			int c = s.nextInt();
-			roots(a, b, c);
+			System.out.println("Enter the temperature in fahrenheit:");
+			double f = s.nextDouble();
+			System.out.println("Enter the wind speed in miles per hour:");
+			double ws = s.nextDouble();
+			if (Math.abs(f) > 50 || ws > 120 || ws < 3) {
+				System.out.println("Enter correct input");
+			} else {
+				windChill(f, ws);
+			}
 		} catch (Exception e) {
-			System.out.println("Enter correct input");
+			System.out.println("Enter proper input");
 		}
 		s.close();
 	}
