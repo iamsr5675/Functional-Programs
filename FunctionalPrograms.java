941d44d61736eb6e84dd178661864a3c3630c214
iamsr5675 <saurabhranjan5675@gmail.com>
2021-08-22 02:57:26 +0530

    Sum of three Integer adds to ZERO

--- FunctionalPrograms.java
+++ FunctionalPrograms.java
@@ -1,121 +1,47 @@
 package com.bridgelabz.FunctionalPrograms;
 
-import java.io.PrintWriter;
 import java.util.Scanner;
 public class FunctionalPrograms 
 {
+	public static int findDistinctTriplets(int b[],int length)
+	{
+		System.out.println();
+		int i, j, k, count=0;
+		for(i=0; i<length; i++)
+		{
+			for(j=i+1; j<length; j++)
+			{
+				for(k=j+1; k<length; k++)
+				{
+					if (b[i] + b[j] + b[k] == 0)
+					{
+						System.out.println(b[i] + " " + b[j] + " " + b[k]);
+						count++;
+					}
+				}
+			}
+		}
+		return count;
+	}
+
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in);      
-		System.out.println("Enter number of rows: ");
-		int m = sc.nextInt();
-		System.out.println("Enter number of columns: ");
-		int n = sc.nextInt(); 
-		        
-		//calling methods
-		int [][] a  =arrayInt(m,n);
-		double [][] b  =arrayDouble(m,n);
-		String [][] c  =arrayBoolean(m,n);
-		        
-		//calling display method
-		display(a,b,c,m,n);
-		        
-	}
-	
-		public static int[][] arrayInt(int m,int n)
-		{
-			int a[][]=new int[m][n];
-			Scanner sc = new Scanner(System.in);    
-			System.out.println();
-			System.out.println("Integer Array");
-	
-			for(int i = 0; i<m; i++)
-			{
-				for (int j = 0; j<n; j++)
-			    {
-					a[i][j] = sc.nextInt();
-			    }   
-			}
-			return a;
-		}
-		
-		public static double[][] arrayDouble(int m,int n)
-		{
-			double b[][]=new double[m][n];
-			Scanner sc = new Scanner(System.in);    
-			System.out.println();
-			System.out.println("Double Array");
-	
-			for(int i = 0; i<m; i++)
-			{
-				for (int j = 0; j<n; j++)
-			    {
-					b[i][j] = sc.nextInt();
-			    }   
-			}
-			return b;
-		}
-		
-		public static String[][] arrayBoolean(int m,int n)
-		{
-		String c[][]=new String[m][n];
-		Scanner sc = new Scanner(System.in);    
+		Scanner sc = new Scanner(System.in);
 		System.out.println();
-		System.out.println("Boolean Array");
-	
-			for(int i = 0; i<m; i++)
-			{
-				for (int j = 0; j<n; j++)
-				{
-					c[i][j] = sc.nextLine();
-				}   
-			}
-		return c;
+
+		System.out.print("Enter Size of Array :");
+		int size = sc.nextInt();
+		int arr[] = new int [size];
+
+		for(int i=0; i<arr.length; i++)
+		{
+			System.out.print("Enter "+(i+1)+" Value :");
+			arr[i] = sc.nextInt();
 		}
-	
-		//display method
-		public static void display(int[][]a,double[][]b,String[][]c,int m,int n)
-		{
-		PrintWriter pw=new PrintWriter(System.out,true);
-		
-		//display integers
+
+		FunctionalPrograms t1 = new FunctionalPrograms();
+		int sum = t1.findDistinctTriplets(arr,size);
+		System.out.println("No.of Tripltes :"+sum);
 		System.out.println();
-		pw.println("2D ARRAY INTEGER");
-		
-		for(int i = 0; i<m; i++)
-		{
-			for (int j = 0; j<n; j++)
-	        {
-				pw.print("\t"+a[i][j]+" ");
-	        } 
-	     pw.println("\t");
-	    }
-		 
-		 
-		//display double
-		System.out.println();
-		pw.println("2D ARRAY DOUBLE");
-			
-		for(int i = 0; i<m; i++)
-		{
-			for (int j = 0; j<n; j++)
-		    {
-				pw.print("\t"+b[i][j]+" ");
-		    } 
-		pw.println("\t");
-		}
-			 
-		//display boolean
-		System.out.println();
-		pw.println("2D ARRAY BOOLEAN");
-				
-		for(int i = 0; i<m; i++)
-		{
-			for (int j = 0; j<n; j++)
-			{
-				pw.print("\t"+c[i][j]+" ");
-			} 
-		pw.println("\t");
-		}
 	}
 }
